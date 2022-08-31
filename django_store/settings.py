796a15253b5596e191deac221909c3f6f56192ed
MohamadAlgazli <brawjym@gmail.com>
2022-09-01 00:27:01 +0300

    Admin panel (1)

--- django_store/settings.py
+++ django_store/settings.py
@@ -114,6 +114,10 @@ USE_I18N = True
 USE_TZ = True
 
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = 'media/'
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
